8311cba02949ff7f04e8da1d8157df711443544b
Kezia Meilany Tandapai <keziameilany2005@gmail.com>
2024-12-10 15:02:50 +0700

    attempt deploy server #12

--- server/main.py
+++ server/main.py
@@ -118,4 +118,4 @@ def handle_attendance():
     return jsonify({"name": name, "accuracy": accuracy})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
